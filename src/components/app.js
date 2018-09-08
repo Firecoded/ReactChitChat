50db0334ed24f75b3743d763378fbe32a40f9ae7
firecoded <yepyepguru@gmail.com>
2018-09-07 17:49:04 -0700

    Pulling and adding to firebase"

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 import {Route} from 'react-router-dom';
 import Home from './home';
 import Chat from './chat';
+import SetName from './set_name';
 
 
 
@@ -11,6 +12,7 @@ const App = () => (
     <div className = 'container blue-grey lighten-5'>
         <Route path = '/' exact component = {Home}/>
         <Route path = '/chat' component = {Chat}/>
+        <Route path ='/set-name' component = {SetName}/>
     </div>
 );
 
